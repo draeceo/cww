659332e13fbd836e7aba51810d249a02511422ca
Emmett Steven <emmett@draecodev.com>
2020-08-11 11:43:53 -0400

    Add sign out method to admin access

--- src/database/AdminAccess.js
+++ src/database/AdminAccess.js
@@ -7,6 +7,10 @@ export const signInAdmin = (email, password) => {
   return auth.signInWithEmailAndPassword(email, password);
 }
 
+export const signOutAdmin = () => {
+  return auth.signOut();
+}
+
 export const isSignedIn = () => {
   return auth.currentUser != null;
 }
